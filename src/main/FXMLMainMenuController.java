26adc8e9d22459c73992abef4c94af625b589107
wilso <wilso@LAPTOP-L7MLOA1K>
2022-05-21 13:46:06 -0600

    Se crea el proyecto en Java con el que se trabajará en el proyecto

---
+++ src/main/FXMLMainMenuController.java
@@ -0,0 +1,97 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML2.java to edit this template
+ */
+package main;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
+
+/**
+ *
+ * @author Profesor Gilberth Chaves A <gchavesav@ucr.ac.cr>
+ */
+public class FXMLMainMenuController implements Initializable {
+
+    private Label label;
+    @FXML
+    private BorderPane bp;
+    @FXML
+    private Button btnHome;
+    @FXML
+    private AnchorPane ap;
+    @FXML
+    private Button btnExit;
+    @FXML
+    private Text txtMessage;
+    @FXML
+    private Button BTN_Priority;
+    @FXML
+    private Button BTN_ReplaceQueue;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }
+
+    private void loadPage(String page) {
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource(page + ".fxml"));
+        } catch (IOException ex) {
+            Logger.getLogger(FXMLMainMenuController.class.getName());
+        }
+        bp.setCenter(root);
+    }
+
+    public static void loadPage(URL ui, BorderPane bp) {
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(ui);
+        } catch (IOException ex) {
+            Logger.getLogger(FXMLMainMenuController.class.getName());
+        }
+
+        bp.setCenter(null);
+        bp.setRight(null);
+        bp.setBottom(null);
+        bp.setTop(null);
+        bp.setCenter(root);
+    }
+
+    @FXML
+    private void Home(MouseEvent event) {
+        this.txtMessage.setText("Proyecto-Grupo6");
+        this.bp.setCenter(ap);
+    }
+   
+
+    @FXML
+    private void Exit(MouseEvent event) {
+        System.exit(0); 
+
+    }
+
+    @FXML
+    private void BTN_PriorityOnAction(ActionEvent event) {
+        
+    }
+
+    @FXML
+    private void BTN_ReplaceQueueOnAction(ActionEvent event) {
+          
+    }
+
+}
